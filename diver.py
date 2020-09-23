7e0223ea9070c314e75139e83fd0fe0c04125ce8
Titouan <titouan@guiochet.com>
2020-09-23 14:11:35 +0200

    Diver position

---
+++ diver.py
@@ -0,0 +1,23 @@
+from pypot.creatures import PoppyErgoJr
+poppy = PoppyErgoJr(
+    simulator='vrep', 
+    scene='poppy_ergo_jr_holder.ttt', 
+    camera='dummy'
+    )
+
+# Block the motor to move by hand
+poppy.m1.compliant = False
+poppy.m2.compliant = False
+poppy.m3.compliant = False
+poppy.m4.compliant = False
+poppy.m5.compliant = False
+poppy.m6.compliant = False
+
+# Position : Diver
+def position_diver():
+    poppy.m2.goal_position = 0
+    poppy.m3.goal_position = 0
+    poppy.m5.goal_position = 0
+    poppy.m6.goal_position = 0
+    
+position_diver()
