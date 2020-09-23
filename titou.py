18edd5dd0dd40c95b2d2bd3d1d85381a4ef7d660
Titouan <titouan@guiochet.com>
2020-09-23 12:01:17 +0200

    Start and stop position

--- titou.py
+++ titou.py
@@ -5,4 +5,19 @@ poppy = PoppyErgoJr(
     camera='dummy'
     )
 
-print('hello world')
+# Block the motor to move by hand
+poppy.m1.compliant = False
+poppy.m2.compliant = False
+poppy.m3.compliant = False
+poppy.m4.compliant = False
+poppy.m5.compliant = False
+poppy.m6.compliant = False
+
+# Position : Start and stop
+def position_start_stop():
+    poppy.m2.goal_position = 0
+    poppy.m3.goal_position = 0
+    poppy.m5.goal_position = 0
+    poppy.m6.goal_position = 0
+    
+position_start_stop()
