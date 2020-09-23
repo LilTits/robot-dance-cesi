e880aae88c7ab76ec3a04fd9f154463498460728
sachgar <sacha.garnier@live.fr>
2020-09-23 14:22:30 +0200

    Ceci un Looping

---
+++ loping.py
@@ -0,0 +1,14 @@
+from pypot.creatures import PoppyErgoJr
+import time
+poppy = PoppyErgoJr(
+    simulator='vrep', 
+    scene='poppy_ergo_jr_holder.ttt', 
+    camera='dummy'
+    )
+
+poppy.m1.goto_position(0,0)
+poppy.m2.goto_position(0,0)
+poppy.m3.goto_position(0,0)
+poppy.m4.goto_position(0,0)
+poppy.m5.goto_position(0,0)
+poppy.m6.goto_position(0,0)
