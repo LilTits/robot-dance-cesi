8e52c1795244c4617adc0221153233261990381e
max <maximw85@gmail.com>
2020-09-23 14:10:13 +0200

    default pos in main

--- max.py
+++ max.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 23 11:45:01 2020
-
-@author: cesi
-"""
+import time
 from pypot.creatures import PoppyErgoJr
 poppy = PoppyErgoJr(
     simulator='vrep', 
     scene='poppy_ergo_jr_holder.ttt', 
     camera='dummy'
     )
+
+#position 2 : skywatching
+poppy.m1.goto_position(0,0)
+poppy.m2.goto_position(0,0)
+poppy.m3.goto_position(0,0)
+poppy.m4.goto_position(0,0)
+poppy.m5.goto_position(-90,0, wait=True)
+poppy.m6.goto_position(0,0, wait=True)
+time.sleep(3)
