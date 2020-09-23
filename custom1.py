3265f1faa10133778a12f66fda98b89ef4165734
max <maximw85@gmail.com>
2020-09-23 14:56:43 +0200

    custom1 finished

--- custom1.py
+++ custom1.py
@@ -5,9 +5,19 @@ poppy = PoppyErgoJr(
     camera='dummy'
     )
 
+#custom 1
+
 poppy.m1.goto_position(0,0)
 poppy.m2.goto_position(0,0)
 poppy.m3.goto_position(0,0)
 poppy.m4.goto_position(0,0)
 poppy.m5.goto_position(0,0)
 poppy.m6.goto_position(0,0)
+
+poppy.m1.goto_position(90,50, wait=True)
+poppy.m1.goto_position(-90,25, wait=True)
+poppy.m4.goto_position(90,25)
+poppy.m2.goto_position(90,25)
+poppy.m1.goto_position(90,25, wait=True)
+poppy.m2.goto_position(-90,25)
+poppy.m4.goto_position(-90,25)
